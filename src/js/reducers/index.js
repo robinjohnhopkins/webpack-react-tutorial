5a28633aca9fdb4a3f50e3734b6b55b08bf366b7
Robin Hopkins <robinjohnhopkins@hotmail.com>
2019-04-13 17:06:26 +0100

    simple reducer

---
+++ src/js/reducers/index.js
@@ -0,0 +1,8 @@
+// src/js/reducers/index.js
+const initialState = {
+    articles: []
+  };
+  function rootReducer(state = initialState, action) {
+    return state;
+  };
+  export default rootReducer;
